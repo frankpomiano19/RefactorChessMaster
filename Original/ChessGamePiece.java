3827146c8784a37f827295b4f160122362bc0f76
BrandomNV <noriegaverab@gmail.com>
2022-08-18 12:40:57 -0500

    clase Enemy

--- Original/ChessGamePiece.java
+++ Original/ChessGamePiece.java
@@ -3,6 +3,7 @@ package Original;
 import java.awt.Color;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
+
 // -------------------------------------------------------------------------
 /**
  * Abstract class that is used to represent a game piece on the chess board.
@@ -19,6 +20,7 @@ public abstract class ChessGamePiece{
     private boolean             skipMoveGeneration;
     private int                 pieceColor;
     private ImageIcon           pieceImage;
+    private Enemy               isAEnemy = new Enemy();
     /**
      * The list of possible moves for this piece. Updated when actions involving
      * this piece occur. (created, moved, selected, etc)
@@ -35,15 +37,15 @@ public abstract class ChessGamePiece{
     /**
      * Represents a black piece as an int
      */
-    static final int            BLACK      = 0;
+    public static final int            BLACK      = 0;
     /**
      * Represents a white piece as an int
      */
-    static final int            WHITE      = 1;
+    public static final int            WHITE      = 1;
     /**
      * Represents a piece that has not been assigned a color
      */
-    static final int            UNASSIGNED = -1;
+    public static final int            UNASSIGNED = -1;
     // ----------------------------------------------------------
     /**
      * Create a new GamePiece object.
@@ -366,6 +368,13 @@ public abstract class ChessGamePiece{
         }
         return moves;
     }
+
+    // --------------------------
+    public boolean isEnemy( ChessGameBoard board, int row, int col ) {
+        return isAEnemy.isEnemy(board, row, col, this.getColorOfPiece());
+    }
+
+
     // ----------------------------------------------------------
     /**
      * Calculates and returns moves in the south-east direction relative to this
@@ -650,53 +659,9 @@ public abstract class ChessGamePiece{
         }
         return false;
     }
-    // ----------------------------------------------------------
-    /**
-     * Determines if the row and column contains an enemy piece. This is defined
-     * in GamePiece and not ChessGameBoard because different pieces have
-     * different enemies depending on their colors.
-     *
-     * @param row
-     *            row of the GamePiece
-     * @param col
-     *            column of the GamePiece
-     * @param board
-     *            the board to check
-     * @return true if it is an enemy piece, false if not
-     */
-    public boolean isEnemy( ChessGameBoard board, int row, int col ){
-        if ( row > 7 || col > 7 || row < 0 || col < 0 ){
-            return false;
-        }
-        ChessGamePiece enemyPiece =
-            board.getCell( row, col ).getPieceOnSquare() == null
-                ? null
-                : board.getCell( row, col ).getPieceOnSquare();
-        if ( enemyPiece == null
-            || this.getColorOfPiece() == ChessGamePiece.UNASSIGNED
-            || enemyPiece.getColorOfPiece() == ChessGamePiece.UNASSIGNED ){
-            return false;
-        }
-        if ( this.getColorOfPiece() == ChessGamePiece.WHITE ){
-            if ( enemyPiece.getColorOfPiece() == ChessGamePiece.BLACK ){
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            if ( enemyPiece.getColorOfPiece() == ChessGamePiece.WHITE ){
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
+
+
+
     // ----------------------------------------------------------
     /**
      * Gets a list of GamePieces that can currently attack this game piece.
